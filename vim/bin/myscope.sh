6600ced49fd4c44d15713577c14196522974df91
Maximilian Fricke <maximilian.fricke@airties.com>
2015-09-02 16:01:13 +0300

    Make myscope accept max depth

--- vim/bin/myscope.sh
+++ vim/bin/myscope.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 print_usage () {
-    echo "Usage: myscope.sh <project path>"
+    echo "Usage: myscope.sh <project path> [depth]"
 }
 
 # Parse arguments
 if [ ! -d $1 ]; then
     print_usage
-    exit
+    exit 1
+fi
+
+# Check for depth
+if [ ! -z $2 ]; then
+    if [[ ! $2 =~ ^[0-9]+$ ]]; then
+        print_usage
+        exit 1
+    else
+        DEPTH=$2
+    fi
 fi
 
 # Get project location
@@ -24,7 +34,11 @@ cd $PRJ_DIR/.myscope
 
 # Find files to be indexed
 for EXT in "c" "h" "cpp" "hpp" "C" "H"; do
-    find $PRJ_DIR -name "*.${EXT}" >> myscope.files
+    if [ ! -z $DEPTH ]; then
+        find $PRJ_DIR -maxdepth $DEPTH -name "*.${EXT}" >> myscope.files
+    else
+        find $PRJ_DIR -name "*.${EXT}" >> myscope.files
+    fi
 done
 
 # Build cscope references
