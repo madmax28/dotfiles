ca65ac267801636f6590dd5d44a9c13ed0e0c89a
Maximilian Fricke <mfricke2808@gmail.com>
2025-02-04 10:28:41 +0100

    lualine: show relative paths

--- nvim/lua/config/plugins/lualine.lua
+++ nvim/lua/config/plugins/lualine.lua
@@ -1,1 +1,18 @@
-require('lualine').setup {}
+require('lualine').setup {
+    sections = {
+        lualine_c = {
+            {
+                'filename',
+                path = 1,
+            },
+        },
+    },
+    inactive_sections = {
+        lualine_c = {
+            {
+                'filename',
+                path = 1,
+            },
+        },
+    },
+}
