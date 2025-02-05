ea3539a5b46d7c9ed138407fd5604db4b9868ab4
Maximilian Fricke <mfricke2808@gmail.com>
2025-02-05 21:01:59 +0100

    port a mapping to lua

--- nvim/lua/config/grep.lua
+++ nvim/lua/config/grep.lua
@@ -2,6 +2,4 @@ if vim.fn.executable('rg') == 1 then
     vim.o.grepprg = "rg --vimgrep --hidden --glob '!.git' --sort=path"
 end
 
-vim.cmd([[
-    nnoremap <leader>G :grep! -w <c-r><c-w><cr>
-]])
+vim.keymap.set('n', '<leader>G', ':grep! -w <c-r><c-w><cr>')
