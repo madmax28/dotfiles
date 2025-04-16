e5dce6cbc0a59e8a9f549c127960484fead05e31
Maximilian Fricke <mfricke2808@gmail.com>
2025-04-16 10:50:39 +0200

    pylsp: set maxLineLength to 100

--- nvim/lua/config/lsp/python.lua
+++ nvim/lua/config/lsp/python.lua
@@ -1,3 +1,13 @@
 require 'lspconfig'.pylsp.setup {
     capabilities = require('cmp_nvim_lsp').default_capabilities(),
+    settings = {
+        pylsp = {
+            plugins = {
+                pycodestyle = {
+                    ignore = { 'W391' },
+                    maxLineLength = 100,
+                },
+            },
+        },
+    },
 }
