edc291783931651f5d2b8d526e4d897b4f8a673d
Maximilian Fricke <mfricke2808@gmail.com>
2025-04-24 09:38:50 +0200

    ripgrep: ignore tags file

--- nvim/lua/config/grep.lua
+++ nvim/lua/config/grep.lua
@@ -1,5 +1,5 @@
 if vim.fn.executable('rg') == 1 then
-    vim.o.grepprg = "rg --vimgrep --hidden --glob '!.git' --sort=path"
+    vim.o.grepprg = "rg --vimgrep --hidden --glob '!.git' --glob '!tags' --sort=path"
 end
 
 vim.keymap.set('n', '<leader>G', ':grep! -w <c-r><c-w><cr>')
