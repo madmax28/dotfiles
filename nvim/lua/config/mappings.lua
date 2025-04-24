9094d02d36336bf4c7636b0c444462537e2a7913
Maximilian Fricke <mfricke2808@gmail.com>
2025-04-24 09:36:47 +0200

    add :tselect mapping

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -42,3 +42,5 @@ vim.keymap.set('n', '<c-j>', '<c-w>j', { silent = true })
 vim.keymap.set('n', '<c-k>', '<c-w>k', { silent = true })
 vim.keymap.set('n', '<c-h>', '<c-w>h', { silent = true })
 vim.keymap.set('n', '<c-l>', '<c-w>l', { silent = true })
+
+vim.keymap.set('n', 'gt', ':tselect <c-r><c-w><cr>', { silent = true })
